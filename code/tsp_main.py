95553f4d4610386b6d449220cdbdbdac74a293d8
Filip Gajowniczek <filip.r.gajowniczek@gmail.com>
2019-11-09 22:05:34 -0600

    Made use of more sophisticated argument parsing from argparse

--- code/tsp_main.py
+++ code/tsp_main.py
@@ -2,16 +2,20 @@ import os
 import sys
 import csv
 import numpy as np
+import argparse
 
 # Script entry point
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) < 7:
-        sys.exit("Usage: {} -inst <filename> -alg [BnB|Approx|LS1|LS2] -time <cutoff_in_seconds> [-seed <random_seed>]".format(args[0]))
 
-    input_filepath = sys.argv[2]
-    alg_type = sys.argv[4]
-    t_cutoff = sys.argv[6]
+    # Instantiate and configure argument parser object
+    parser = argparse.ArgumentParser(description='TSP argument parser')
+    parser.add_argument( '-inst', required=True )
+    parser.add_argument( '-alg', required=True )
+    parser.add_argument( '-time', required=True, type=int )
+    parser.add_argument( '-seed', type=int )
 
-    if (alg_type == 'LS1' or alg_type == 'LS2' ) and len(args) < 9 :
-        sys.exit( "Usage: {}".format(args[0])+"\t-seed parameter must be provided for -alg={}".format(args[4]) )
+    # Command argument parser to process command line arguments
+    args = parser.parse_args()
+
+    if (args.alg == 'LS1' or args.alg == 'LS2' ) and args.seed == None:
+        sys.exit( "Usage: {}".format(parser.prog)+"\t-seed parameter must be provided for -alg={}".format(args.alg) )
