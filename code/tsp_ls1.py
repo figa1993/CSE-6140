183b52801cf7782cb01ef12f3304a4919ef74ec9
Filip Gajowniczek <filip.r.gajowniczek@gmail.com>
2019-11-21 19:42:43 -0600

    Added some comments to tsp_ls1.py

--- code/tsp_ls1.py
+++ code/tsp_ls1.py
@@ -43,6 +43,8 @@ def greedy_heuristic(dist_matrix,index_start):
             rem_nodes[next_hop] = 0
             total_cost += dist
         else:
+            # TODO: instead, pass a list of flags into get_min_cost indicating which nodes have not been visited
+            # and perform a single linear search which finds node s.t. cost is minimal AND it has not been visited
             dist_matrix[start_index,next_hop].cost = np.inf
     total_cost += initialdist_matrix[route[len(route)-1],route[0]].cost
     route.append(route[0])
@@ -58,6 +60,7 @@ def calculate_route_cost(route,cost_matrix):
 def two_opt(route_matrix,dist_matrix,cost):
     bestroute = route_matrix
     improvement = False
+    # TODO: This loop does not consider flips which include endpoints
     for i in range(1,len(route_matrix)-1):
         for k in range(i+i,len(route_matrix)):
             improved_route = route_matrix[:]
@@ -90,6 +93,7 @@ def ls1( nodes , timeout : int,seed_num ):
     for x in start_nodes:
         print(x)
         print(top_cost)
+        # TODO: Make a deepcopy of the matrix and pass it in on line 97, that way you won't have to recalc at line 105
         route,total_cost = greedy_heuristic(mat,x)
         if total_cost < top_cost:
             top_cost = total_cost
@@ -100,6 +104,8 @@ def ls1( nodes , timeout : int,seed_num ):
         improving = True
         mat = distance_calculator(nodes)
         if time.time()-starttime > timeout:
+            # TODO: is it sufficient just to return the solution? if greedy_heuristic returned a better total_cost
+            # a trace was already added in line 101 and top_cost was updated so the condition on next line is always 0?
             if total_cost < top_cost:
                 trace.add_tracepoint(time.time() - starttime,total_cost)
                 return Solution( total_cost, top_route), trace
@@ -117,5 +123,6 @@ def ls1( nodes , timeout : int,seed_num ):
                 top_cost = total_cost
                 top_route = route
                 trace.add_tracepoint(time.time() - starttime,total_cost)              #print(route,total_cost)
+    # TODO: can the return on the next line actually ever be hit?
     return Solution( top_cost, top_route), trace
 
